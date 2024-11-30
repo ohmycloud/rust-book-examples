f20797e637438a5bf8679e58614ad649350425dd
ohmycloud <ohmycloudy@gmail.com>
2024-11-30 16:05:44 +0800

    add event loop

--- async-rust/ch06-reactive-heater/examples/heater.rs
+++ async-rust/ch06-reactive-heater/examples/heater.rs
@@ -14,6 +14,8 @@ use std::time::{Duration, Instant};
 static TEMP: LazyLock<Arc<AtomicI16>> = LazyLock::new(|| Arc::new(AtomicI16::new(2090)));
 static DESIRED_TEMP: LazyLock<Arc<AtomicI16>> = LazyLock::new(|| Arc::new(AtomicI16::new(2100)));
 static HEAT_ON: LazyLock<Arc<AtomicBool>> = LazyLock::new(|| Arc::new(AtomicBool::new(false)));
+static INPUT: LazyLock<Arc<Mutex<String>>> = LazyLock::new(|| Arc::new(Mutex::new(String::new())));
+static DEVICE_STATE: LazyLock<Arc<DeviceState>> = LazyLock::new(|| Arc::new(DeviceState::new()));
 
 pub struct DisplayFuture {
     pub temp_snapshot: i16,
@@ -124,12 +126,39 @@ where
     callback(result);
 }
 
+pub fn render(temp: i16, desired_temp: i16, heat_on: bool, input: String) {
+    clearscreen::clear().unwrap();
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    println!(
+        "Temperature: {}\nDesired Temp: {}\nHeater On: {}",
+        temp as f32 / 100.0,
+        desired_temp as f32 / 100.0,
+        heat_on
+    );
+    print!("Input: {}", input);
+    handle.flush().unwrap();
+}
+
 #[tokio::main]
 async fn main() {
     let my_callback = |result: i32| {
         println!("The result is: {}", result);
     };
     perform_operation_with_callback(my_callback);
+
+    let _guard = DeviceState.on_key_down(|key| {
+        let mut input = INPUT.lock().unwrap();
+        input.push_str(&key.to_string());
+        std::mem::drop(input);
+        render(
+            TEMP.load(Ordering::Acquire),
+            DESIRED_TEMP.load(Ordering::SeqCst),
+            HEAT_ON.load(Ordering::SeqCst),
+            INPUT.lock().unwrap().clone(),
+        );
+    });
+
     let display = tokio::spawn(async {
         DisplayFuture::new().await;
     });
