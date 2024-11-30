53185596cbf8fa70129265510eb3490004e44bd2
ohmycloud <ohmycloudy@gmail.com>
2024-11-30 14:29:09 +0800

    reactive heater system

---
+++ async-rust/ch06-reactive-heater/examples/heater.rs
@@ -0,0 +1,131 @@
+use core::sync::atomic::Ordering;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::sync::LazyLock;
+use std::sync::atomic::{AtomicBool, AtomicI16};
+use std::task::Context;
+use std::task::Poll;
+use std::time::{Duration, Instant};
+
+static TEMP: LazyLock<Arc<AtomicI16>> = LazyLock::new(|| Arc::new(AtomicI16::new(2090)));
+static DESIRED_TEMP: LazyLock<Arc<AtomicI16>> = LazyLock::new(|| Arc::new(AtomicI16::new(2100)));
+static HEAT_ON: LazyLock<Arc<AtomicBool>> = LazyLock::new(|| Arc::new(AtomicBool::new(false)));
+
+pub struct DisplayFuture {
+    pub temp_snapshot: i16,
+}
+
+impl DisplayFuture {
+    pub fn new() -> Self {
+        DisplayFuture {
+            temp_snapshot: TEMP.load(Ordering::SeqCst),
+        }
+    }
+}
+
+impl Future for DisplayFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let current_snapshot = TEMP.load(Ordering::SeqCst);
+        let desired_temp = DESIRED_TEMP.load(Ordering::SeqCst);
+        let head_on = HEAT_ON.load(Ordering::SeqCst);
+
+        if current_snapshot == self.temp_snapshot {
+            cx.waker().wake_by_ref();
+            return Poll::Pending;
+        }
+        if current_snapshot < desired_temp && head_on == false {
+            HEAT_ON.store(true, Ordering::SeqCst);
+        } else if current_snapshot > desired_temp && head_on == true {
+            HEAT_ON.store(false, Ordering::SeqCst);
+        }
+        clearscreen::clear().unwrap();
+        println!(
+            "Temperature: {}\nDesired Temp: {}\nHeater On: {}",
+            current_snapshot as f32 / 100.0,
+            desired_temp as f32 / 100.0,
+            head_on
+        );
+        self.temp_snapshot = current_snapshot;
+        cx.waker().wake_by_ref();
+        return Poll::Pending;
+    }
+}
+
+pub struct HeaterFuture {
+    pub time_snapshot: Instant,
+}
+
+impl HeaterFuture {
+    pub fn new() -> Self {
+        Self {
+            time_snapshot: Instant::now(),
+        }
+    }
+}
+
+impl Future for HeaterFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if HEAT_ON.load(Ordering::SeqCst) == false {
+            self.time_snapshot = Instant::now();
+            cx.waker().wake_by_ref();
+            return Poll::Pending;
+        }
+        let current_snapshot = Instant::now();
+        if current_snapshot.duration_since(self.time_snapshot) < Duration::from_secs(3) {
+            cx.waker().wake_by_ref();
+            return Poll::Pending;
+        }
+        TEMP.fetch_add(3, Ordering::SeqCst);
+        self.time_snapshot = Instant::now();
+        cx.waker().wake_by_ref();
+        return Poll::Pending;
+    }
+}
+
+pub struct HeatLossFuture {
+    pub time_snapshot: Instant,
+}
+
+impl HeatLossFuture {
+    pub fn new() -> Self {
+        Self {
+            time_snapshot: Instant::now(),
+        }
+    }
+}
+
+impl Future for HeatLossFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let current_snapshot = Instant::now();
+        if current_snapshot.duration_since(self.time_snapshot) > Duration::from_secs(3) {
+            TEMP.fetch_sub(1, Ordering::SeqCst);
+            self.time_snapshot = Instant::now();
+        }
+        cx.waker().wake_by_ref();
+        return Poll::Pending;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let display = tokio::spawn(async {
+        DisplayFuture::new().await;
+    });
+
+    let heat_loss = tokio::spawn(async {
+        HeatLossFuture::new().await;
+    });
+    let heater = tokio::spawn(async {
+        HeaterFuture::new().await;
+    });
+    display.await.unwrap();
+    heat_loss.await.unwrap();
+    heater.await.unwrap();
+}
