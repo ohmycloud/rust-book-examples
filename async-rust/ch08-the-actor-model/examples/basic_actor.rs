49967adc90e7f65eefdf568a16c6721f19c351b0
ohmycloud <ohmycloudy@gmail.com>
2025-08-24 17:34:40 +0800

    build a basic actor

---
+++ async-rust/ch08-the-actor-model/examples/basic_actor.rs
@@ -0,0 +1,27 @@
+use tokio::sync::mpsc::{Receiver, Sender, channel};
+use tokio::sync::oneshot;
+
+struct Message {
+    value: i64,
+}
+
+async fn basic_actor(mut rx: Receiver<Message>) {
+    let mut state = 0;
+
+    while let Some(msg) = rx.recv().await {
+        state += msg.value;
+        println!("Received: {}", msg.value);
+        println!("State: {}", state);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = channel::<Message>(100);
+
+    let _actor_handle = tokio::spawn(basic_actor(rx));
+    for i in 0..10 {
+        let msg = Message { value: i };
+        tx.send(msg).await.unwrap();
+    }
+}
