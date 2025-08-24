5802e84c2368841e43bf2e34fc7a8616fce2da4b
ohmycloud <ohmycloudy@gmail.com>
2025-08-24 21:00:33 +0800

    performance of mutexes

--- async-rust/ch08-the-actor-model/examples/basic_actor.rs
+++ async-rust/ch08-the-actor-model/examples/basic_actor.rs
@@ -1,5 +1,7 @@
+use std::sync::Arc;
+
 use tokio::sync::mpsc::{Receiver, channel};
-use tokio::sync::oneshot;
+use tokio::sync::{Mutex, oneshot};
 
 struct Message {
     value: i64,
@@ -59,8 +61,36 @@ async fn oneshot_message() {
     }
 }
 
+async fn actor_replacement(state: Arc<Mutex<i64>>, value: i64) -> i64 {
+    let mut state = state.lock().await;
+    *state += value;
+    *state
+}
+
+async fn actors_vs_mutexes() {
+    let state = Arc::new(Mutex::new(0));
+    let mut handles = Vec::new();
+
+    let now = tokio::time::Instant::now();
+
+    for i in 0..100_000_000 {
+        let state_ref = state.clone();
+        let future = async move {
+            let handle = tokio::spawn(async move { actor_replacement(state_ref, i).await });
+            let _ = handle.await.unwrap();
+        };
+        handles.push(tokio::spawn(future));
+    }
+
+    for handle in handles {
+        let _ = handle.await.unwrap();
+    }
+    println!("Elapsed: {:?}", now.elapsed());
+}
+
 #[tokio::main]
 async fn main() {
     send_message().await;
     oneshot_message().await;
+    actors_vs_mutexes().await;
 }
