2deb3fa5ff4bbe1dac74993cdd36c9f8ed33d1e6
ohmycloud <ohmycloudy@gmail.com>
2025-08-26 18:14:23 +0800

    building sleep future

---
+++ async-rust/ch10-building-an-async-server/async_runtime/src/sleep.rs
@@ -0,0 +1,31 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+    time::{Duration, Instant},
+};
+
+pub struct Sleep {
+    when: Instant,
+}
+
+impl Sleep {
+    pub fn new(duration: Duration) -> Self {
+        Sleep {
+            when: Instant::now() + duration,
+        }
+    }
+}
+
+impl Future for Sleep {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let now = Instant::now();
+        if self.when <= now {
+            Poll::Ready(())
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
