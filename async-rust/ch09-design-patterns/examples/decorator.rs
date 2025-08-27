7d8dfb5d46e26fa03ae477887dab571b9dfe5b08
ohmycloud <ohmycloudy@gmail.com>
2025-08-27 17:25:40 +0800

    the decorator pattern

---
+++ async-rust/ch09-design-patterns/examples/decorator.rs
@@ -0,0 +1,40 @@
+trait Greeting {
+    fn greet(&self) -> String;
+}
+
+struct HelloWorld;
+
+impl Greeting for HelloWorld {
+    fn greet(&self) -> String {
+        "Hello, World!".to_string()
+    }
+}
+
+impl Greeting for &HelloWorld {
+    fn greet(&self) -> String {
+        "Hello, World!".to_string()
+    }
+}
+
+struct ExcitedGreeting<T> {
+    inner: T,
+}
+
+impl<T> ExcitedGreeting<T> {
+    fn greet(&self) -> String
+    where
+        T: Greeting,
+    {
+        let mut greeting = self.inner.greet();
+        greeting.push_str("I'm so excited to be in Rust!");
+        greeting
+    }
+}
+
+fn main() {
+    let raw_one = HelloWorld;
+    let raw_two = HelloWorld;
+    let decorated = ExcitedGreeting { inner: raw_two };
+    println!("{}", raw_one.greet());
+    println!("{}", decorated.greet());
+}
