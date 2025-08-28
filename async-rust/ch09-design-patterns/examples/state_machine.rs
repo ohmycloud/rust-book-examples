c3c847c1ac88dfc23b1f720dc814b28a064d9c39
ohmycloud <ohmycloudy@gmail.com>
2025-08-28 12:52:03 +0800

    wrap a future in state

--- async-rust/ch09-design-patterns/examples/state_machine.rs
+++ async-rust/ch09-design-patterns/examples/state_machine.rs
@@ -1,3 +1,8 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
 enum State {
     On,
     Off,
@@ -22,6 +27,35 @@ impl State {
             _ => {
                 println!("No transition possible, staying in the current state");
                 self
+            }
+        }
+    }
+}
+
+struct StateFuture<F: Future, X: Future> {
+    pub state: State,
+    pub on_future: F,
+    pub off_future: X,
+}
+
+impl<F: Future, X: Future> Future for StateFuture<F, X> {
+    type Output = State;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.state {
+            State::On => {
+                let inner = unsafe { self.map_unchecked_mut(|s| &mut s.on_future) };
+
+                let _ = inner.poll(cx);
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+            State::Off => {
+                let inner = unsafe { self.map_unchecked_mut(|s| &mut s.off_future) };
+
+                let _ = inner.poll(cx);
+                cx.waker().wake_by_ref();
+                Poll::Pending
             }
         }
     }
